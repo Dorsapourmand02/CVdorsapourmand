0eef6cb98c29d2117303de31a53d6e69c4c8683e
Dori <dorsa@DorsatekiMacBook-Air.local>
2025-05-18 23:37:24 +0330

    NAVBAR ADDED

---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../Navbar/Navbar.css'
+
+function Navbar() {
+  return (
+    <nav>
+        <div className="nav-title">
+            <h4>DORSA POURMAND</h4>
+        </div>
+        <div className="nav-part">
+            <ul>
+                <li><a href="#">CONTACT ME</a></li>
+                <li><a href="#">MY PROJECTS</a></li>
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
