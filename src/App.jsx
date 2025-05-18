f324355b909dca4a5b9fb1a121d79c6d98ba526e
Dori <dorsa@DorsatekiMacBook-Air.local>
2025-05-19 01:24:34 +0330

    Header created

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './Component/Navbar/Navbar'
+import Header from './Component/Header/Header'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Navbar></Navbar>
+    <>
+      <Navbar />
+      <Header />
+    </>
   )
 }
 
--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import "../Header/Header.css"
 
 function Header() {
   return (
-    <div>
+    <div className='header-container'>
+      <div className="header-title">
+        <h1>Hi I'm</h1>
+        <h2>Dorsa Pourmand</h2>
+        <p>I'm Frontend developer skilled in HTML, CSS, Wordpress, JavaScript, and React. Currently expanding my backend skills using PHP. Open to collaboration and freelance opportunities.</p>
+
+        <div className="header-btn">
+          <a href="#"><button className='contact-btn'>Contact Me</button></a>
+          <a href="#"><button className='cv-btn'>Download CV</button></a>
+        </div>
+      </div>
+
+
         
     </div>
   )
